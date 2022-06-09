e66f6f2e914e33a1cecf532a5cb809fb7d2bbad5
Amir Afshari <a_amirhossein@icloud.com>
2022-06-09 12:42:21 -0400

    adds streetview component

---
+++ Demo1_Geohash/src/components/StreetView.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from "svelte/internal";
+
+	const fenway = { lat: 42.345573, lng: -71.098326 };
+	let el;
+
+	onMount(() => {
+		const panorama = new google.maps.StreetViewPanorama(el, {
+			position: fenway,
+			pov: {
+				heading: 34,
+				pitch: 10,
+			},
+		});
+	});
+</script>
+
+<section class="rounded-lg shadow-xl text-sm p-4 h-52">
+	<div bind:this={el} class="h-full w-full" />
+</section>
