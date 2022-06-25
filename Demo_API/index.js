9f50e04284cdad34ae397a504e1f37a8996ce424
Alex Canales <alexcanales766@gmail.com>
2022-06-25 00:57:51 -0400

    Added eslint

--- Demo_API/index.js
+++ Demo_API/index.js
@@ -1,18 +1,17 @@
+/* eslint-disable no-undef */
 import express from "express";
 import morgan from "morgan";
+import dotenv from 'dotenv/config';
+dotenv.config()
 const app = express();  //Create new instance
 
 import treesRouter from './source/routes/trees.js';
-
 
 const PORT = process.env.PORT || 5000; //Declare the port number
 app.use(express.json()); //allows us to access request body as req.body
 app.use(morgan("dev"));  //enable incoming request logging in dev mode
  
-
-
 app.use('/trees', treesRouter);
-
 
 //Define the endpoint
 app.get("/ping", (req, res) => {  
